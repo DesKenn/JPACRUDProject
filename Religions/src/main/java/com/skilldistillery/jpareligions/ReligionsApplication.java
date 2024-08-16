9e216cd529e07da90836ef0dfa92d54a3153f557
DesKenn <destineek423@gmail.com>
2024-08-16 16:44:24 -0600

    create MVC project

---
+++ Religions/src/main/java/com/skilldistillery/jpareligions/ReligionsApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpareligions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReligionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReligionsApplication.class, args);
+	}
+
+}
---
+++ Religions/src/test/java/com/skilldistillery/jpareligions/ReligionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpareligions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReligionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
